dd67c52984350404791181330f66ebc6f1fc23d7
Alex Athorne <a.thornysort@gmail.com>
2023-01-08 22:18:22 +0000

    Simple test app for testing api cors request

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <PostDisplay />
       </header>
     </div>
   );
 }
+
+
+class PostDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: null,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <button
+          className="square"
+          onClick={() => this.request()}
+        >
+          send request
+        </button>
+        <div>
+          {this.state.posts ? this.state.posts.map((post, ind) => <div key={ind}>{post['content']}</div>) : null}
+        </div>
+      </div>
+    );
+  }
+
+  request() {
+    fetch('http://localhost:8000/posts/accepted?from_index=0&to_index=100')
+      .then(response => response.json())
+      .then(json => {console.log(json); this.setState({ posts: json['posts'] })});
+  }
+}
+
+// this.setState({ post: post['content'] })
 
 export default App;
